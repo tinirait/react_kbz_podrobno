2d61b8be2b7349b142d08ca0cf50da983c0aab3b
tinirait <tinirait@gmail.com>
2023-08-24 15:02:26 +0300

    02 components part_2

--- my-app/src/App.tsx
+++ my-app/src/App.tsx
@@ -2,19 +2,30 @@ import React from 'react';
 import './App.css';
 
 
-
 function App() {
+    console.log("App rendering")
     return (
         <div>
-            This is App Component
+            <AppTitle/>
             <Rating/>
             <Accordion/>
+            <Rating/>
         </div>
 
     )
 }
 
+function AppTitle() {
+    console.log("AppTitle rendering")
+    return (
+        <>
+            This is App Component
+        </>
+    )
+}
+
 function Rating() {
+    console.log("Rating rendering")
     return (
         <div>
             <Star/>
@@ -27,19 +38,46 @@ function Rating() {
 }
 
 function Accordion() {
+    console.log("Accordion rendering")
     return (
         <div>
-            <h3>Меню</h3>
+            <AccordionTitle/>
+            <AccordionBody/>
         </div>
     )
 }
 
-function Star (){
-    return(
+function Star() {
+    console.log("Star rendering")
+    return (
         <div>
             Star
         </div>
     )
 }
+
+
+function AccordionTitle() {
+    console.log("AccordionTitle rendering")
+    return (
+        <>
+            <h3>Меню</h3>
+        </>
+    )
+}
+
+function AccordionBody() {
+    console.log("AccordionBody rendering")
+    return (
+        <>
+            <ul>
+                <li>1</li>
+                <li>2</li>
+                <li>3</li>
+            </ul>
+        </>
+    )
+}
+
 
 export default App;
